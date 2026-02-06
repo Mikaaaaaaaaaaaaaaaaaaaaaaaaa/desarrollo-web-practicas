f5892a19af08a00ec3721abbd20e1cf3b5ce4006
Mika <227877359+Mikaaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2026-02-06 01:03:33 -0300

    Se modificaron algunas cosas y se agregaron algunos ejercicios.

--- bdd1-practicas/whiteout-survival-final/SQL.sql
+++ bdd1-practicas/whiteout-survival-final/SQL.sql
@@ -2,6 +2,7 @@ DROP DATABASE WhiteoutSurvivalFinal;
 CREATE DATABASE WhiteoutSurvivalFinal;
 USE WhiteoutSurvivalFinal;
 
+DROP TABLE IF EXISTS CUENTA;
 CREATE TABLE CUENTA (
 	email VARCHAR(50) PRIMARY KEY,
     celular VARCHAR(20),
@@ -11,6 +12,7 @@ CREATE TABLE CUENTA (
     hora_ultima_conexion DATETIME
 );
 
+DROP TABLE IF EXISTS GOBERNADOR;
 CREATE TABLE GOBERNADOR (
 	identificador_gobernador INTEGER PRIMARY KEY,
     nombre VARCHAR(20),
@@ -19,6 +21,7 @@ CREATE TABLE GOBERNADOR (
     region INTEGER
 );
 
+DROP TABLE IF EXISTS COLONIA;
 CREATE TABLE COLONIA (
 	identificador_colonia INTEGER PRIMARY KEY,
     descripcion VARCHAR(20),
@@ -29,6 +32,7 @@ CREATE TABLE COLONIA (
     FOREIGN KEY (identificador_gobernador) REFERENCES GOBERNADOR(identificador_gobernador)
 );
 
+DROP TABLE IF EXISTS HEROE;
 CREATE TABLE HEROE (
 	identificador_gobernador INTEGER,
     codigo_heroe INTEGER,
@@ -46,11 +50,13 @@ CREATE TABLE HEROE (
     FOREIGN KEY (identificador_gobernador) REFERENCES GOBERNADOR(identificador_gobernador)
 );
 
+DROP TABLE IF EXISTS TIPO_RAREZA_EQUIPO_ESPECIFICO;
 CREATE TABLE TIPO_RAREZA_EQUIPO_ESPECIFICO (
 	identificador_tipo_rareza INTEGER PRIMARY KEY,
     descripcion VARCHAR(20)
 );
 
+DROP TABLE IF EXISTS EQUIPO_ESPECIFICO;
 CREATE TABLE EQUIPO_ESPECIFICO (
 	identificador_equipo_especifico INTEGER PRIMARY KEY,
     cantidad_estrellas INTEGER,
@@ -62,12 +68,14 @@ CREATE TABLE EQUIPO_ESPECIFICO (
     FOREIGN KEY (identificador_tipo_rareza) REFERENCES TIPO_RAREZA_EQUIPO_ESPECIFICO (identificador_tipo_rareza)
 );
 
+DROP TABLE IF EXISTS TIPO_RAREZA;
 CREATE TABLE TIPO_RAREZA (
 	identificador_rareza INTEGER PRIMARY KEY,
     nombre_rareza VARCHAR(20),
     color_rareza VARCHAR(20)
 );
 
+DROP TABLE IF EXISTS EQUIPO_HEROE;
 CREATE TABLE EQUIPO_HEROE (
 	identificador_gobernador INTEGER PRIMARY KEY,
     tipo_equipo VARCHAR(20),
@@ -80,6 +88,7 @@ CREATE TABLE EQUIPO_HEROE (
     FOREIGN KEY (identificador_rareza) REFERENCES TIPO_RAREZA (identificador_rareza)
 );
 
+DROP TABLE IF EXISTS CALENTADOR;
 CREATE TABLE CALENTADOR (
 	identificador_colonia INTEGER PRIMARY KEY,
     nivel_calor INTEGER,
@@ -157,7 +166,6 @@ VALUES (20, 'EvangelinaDos', 'Avatar_fuego', 15, 1060);
 INSERT INTO HEROE (identificador_gobernador, codigo_heroe, especialidad, talento, nombre, nivel_alcanzado, seniority, arma, cantidad_estrellas, cantidad_escoltas, capacidad_tropas)
 VALUES (20, 105, 'Programador', 'Sigilo', 'MikaDos', 5, 'Senior', 'AK47', 10, 10, 50);
 
-
 SELECT gob.nombre AS NOMBRE_GOBERNADOR, h.nombre as NOMBRE_HEROE
 FROM GOBERNADOR gob
 JOIN HEROE h ON h.identificador_gobernador = gob.identificador_gobernador
@@ -166,7 +174,6 @@ ORDER BY h.cantidad_estrellas DESC
 LIMIT 5;
 
 -- Mostrar los gobernadores y las cuentas, que posean todos los equipos de gobernador disponibles (sin importar el nivel) pero que sean épicos.
-
 INSERT INTO CUENTA (email, celular, nombre_usuario, contrasenia, fecha_creacion_cuenta, hora_ultima_conexion)
 VALUES ('Evangelina15@gmail.com', '999999999', 'Evangelina', 'Contrasenia', '2026-01-01', '2026-02-10 15:30:00');
 
@@ -204,3 +211,41 @@ WHERE NOT EXISTS (
     JOIN TIPO_RAREZA_EQUIPO_ESPECIFICO tr ON ee.identificador_tipo_rareza = tr.identificador_tipo_rareza
     WHERE ee.identificador_gobernador = gob.identificador_gobernador AND tr.descripcion != 'epico'
 );
+
+-- Suponer que el poder de una alianza es la suma del poder de sus gobernadores.
+-- Listar un ranking de las 10 alianzas más poderosas.
+DROP TABLE IF EXISTS ALIANZA;
+CREATE TABLE ALIANZA (
+	identificador_alianza INTEGER PRIMARY KEY,
+	nombre VARCHAR(20),
+    descripcion VARCHAR(20)
+);
+
+ALTER TABLE GOBERNADOR
+ADD COLUMN poder INTEGER;
+
+UPDATE GOBERNADOR gob
+SET gob.poder = (
+	SELECT SUM(h.cantidad_estrellas)
+	FROM HEROE h
+    WHERE h.identificador_gobernador = identificador_gobernador
+);
+
+ALTER TABLE GOBERNADOR
+ADD COLUMN identificador_alianza INTEGER,
+ADD FOREIGN KEY (identificador_alianza) REFERENCES ALIANZA(identificador_alianza);
+
+SELECT a.nombre, SUM(gob.poder) AS alianzas_mas_poderosas
+FROM ALIANZA a
+JOIN GOBERNADOR gob ON gob.identificador_alianza = a.identificador_alianza
+GROUP BY a.nombre
+ORDER BY alianzas_mas_poderosas DESC
+LIMIT 10;
+
+-- Mostrar un reporte de las regiones con cantidad de cuentas activas en este momento y poder acumulado al momento.
+SELECT gob.region, COUNT(cue.email) AS CUENTAS_ACTIVAS, SUM(h.cantidad_estrellas) AS PODER_ACUMULADO
+FROM CUENTA cue	
+JOIN COLONIA col ON col.email = cue.email
+JOIN GOBERNADOR gob ON col.identificador_gobernador = gob.identificador_gobernador
+JOIN HEROE h ON h.identificador_gobernador = gob.identificador_gobernador
+GROUP BY gob.region;
